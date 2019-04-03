98b71be9bb82e8a3a45e6095b18d68e9d2e4f950
bhlt1998 <34224727+bhlt1998@users.noreply.github.com>
2019-04-03 23:05:44 +0800

    Delete test01_InstallPackage.py

--- test01_InstallPackage.py
+++
@@ -1,8 +0,0 @@
-from com.android.monkeyrunner import MonkeyRunner,MonkeyDevice
-device = MonkeyRunner.waitForConnection()
-if not device:
-    print("Please connect a device to start!")
-else:
-    print("Install package test start!")
-device.installPackage("E:/dontstop/JellyFishBig.apk")
-print("Install Finish.")
