c24ce663bcef01ae0bd8897bfa479bb350ec7d8e
Bill Dior <sick.kitter@hotmail.com>
2019-05-15 00:43:39 +0800

    拖拽的同时发送对应信息，更新部分功能函数

--- GUI/pyqt/Test_Ui_Functions.py
+++ GUI/pyqt/Test_Ui_Functions.py
@@ -2,19 +2,13 @@ import Monkey
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import *
+import socket
+import threading
+import os
+import _thread as thread
 from PyQt5.QtWidgets import QFileDialog, QMessageBox, QDockWidget, QListWidget
 from PyQt5.QtGui import *
-class TestUiFunctions():
-    def __init__(self):
-        a = 0
-    def close_monkeyrunner():
-        Monkey.close()
-    def close_model():
-        sendsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        port = 9999
-        host = '127.0.0.1'
-        sendsocket.sendto(1,(port,host))
-    #一些常数和错误处理#
+class TestUiFunctionsClass(object):
     bool_ever_click_connect_button = 0
     bool_is_device_connected_successful = 0
     min_click_times = 1
@@ -29,24 +23,53 @@ class TestUiFunctions():
     max_drag_interval_time = 99.9
     max_pos_x = 1000
     max_pos_y = 1000
-
+    path = os.getcwd()
     exception_count = 0
+    connect_waiting_timer = None
+    def __init__(self):
+        self.thread_start()
+    def add_text(self,text,widget):
+        widget.addItem(text)
+    def close_monkeyrunner(self):
+        Monkey.close()
+    def close_model(self):
+        sendsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        port = 9999
+        host = '127.0.0.1'
+        sendsocket.sendto(1,(port,host))
+
+    #一些常数和错误处理#
+  
     while(1):
         try:
             open('exception_' + str(exception_count) + '.txt')
             exception_count += 1
         except IOError:
             break
-
-    def monkey_runner_initial():
+    def runMonkeyServer(self,lock):
+    ##!!!!!!!!!!!!!!!!!!!!!!!!!!!!此位置改成MonkeyServer.py所在位置
+        ret = os.system(r"monkeyrunner C:\Users\Lenovo\Desktop\Autotest-master\example\MonkeyServer.py")
+        lock.release(self)
+    def monkey_runner_initial(self):
         lock = thread.allocate_lock()
         lock.acquire()
         thread.start_new(runMonkeyServer,(lock,))
-    def runMonkeyServer(lock):
-    ##!!!!!!!!!!!!!!!!!!!!!!!!!!!!此位置改成MonkeyServer.py所在位置
-        ret = os.system(r"monkeyrunner C:\Users\Lenovo\Desktop\Autotest-master\example\MonkeyServer.py")
-        lock.release()
-    def error_message_prompt(error_code):
+    def connect_waiting(self):
+        #nonlocal bool_is_device_connected_successful
+        connect_log = open(os.getcwd() + '\\connectlog.txt','r')
+        content = connect_log.read()
+        if(content == 'True'):
+            bool_is_device_connected_successful = 1
+            #print_text.insert('end','connection successful!')
+            self.connect_waiting_timer.cancel()
+            connect_log.close()
+        else:
+            connect_log.close()
+            self.connect_waiting_timer = threading.Timer(0.5,self.connect_waiting)
+            self.connect_waiting_timer.start()  
+    def thread_start(self):
+        self.connect_waiting_timer = threading.Timer(0.5,self.connect_waiting)
+    def error_message_prompt(self,error_code):
         empty_error_code = 0
         number_error_code = 1
         logic_error_code = 2
@@ -54,67 +77,38 @@ class TestUiFunctions():
         something_else_error_code = 4
         have_not_connect_error_code = 5
         not_successful_connected_error_code = 6
-        if(error_code == 0):#错误信息:空输入
-            tk.messagebox.showinfo('输入错误','输入不能为空或非半角阿拉伯数字，具体输入规范可参考顶部菜单栏的“帮助”按钮')
-        elif(error_code == 1):#错误信息：输入的应该是数字却不是
-            tk.messagebox.showinfo('输入错误','请输入半角阿拉伯正数，具体输入规范可参考顶部菜单栏的“帮助”按钮')
-        elif(error_code == 2):
-            tk.messagebox.showinfo('输入错误','请输入不要超过限定范围，具体输入范围可参考顶部菜单栏的“帮助”按钮')
-        elif(error_code == 3):
-            tk.messagebox.showinfo('输入错误','请正确输入手机分辨率，以半角阿拉伯正数形式输入')
-        elif(error_code == something_else_error_code):
-            tk.messagebox.showinfo('未知错误','发生了一些未知的错误，程序将退出')
-        elif(error_code == have_not_connect_error_code):
-            tk.messagebox.showinfo('提示','请先连接设备！')
-        elif(error_code == not_successful_connected_error_code):
-            tk.messagebox.showinfo('连接错误','还未成功连接设备')
+        #if(error_code == 0):#错误信息:空输入
+
     #常数和错误处理结束#
-    def connect_waiting():
-        nonlocal bool_is_device_connected_successful
-        connect_log = open(os.getcwd() + '\\connectlog.txt','r')
-        content = connect_log.read()
-        nonlocal connect_waiting_timer
-        if(content == 'True'):
-            bool_is_device_connected_successful = 1
-            #print_text.insert('end','connection successful!')
-            connect_waiting_timer.cancel()
-            connect_log.close()
-        else:
-            connect_log.close()
-            connect_waiting_timer = threading.Timer(0.5,connect_waiting)
-            connect_waiting_timer.start()
-            
+      
     def connect(self):
-        nonlocal bool_ever_click_connect_button
-        bool_ever_click_connect_button = 1
-        path = os.getcwd()
+        
         #print_text.insert('end',"TestMethod: waiting for connection..")
         
         #send('connect',ord(path[0]),0,0,0,0,0,0,path[2:])
         Monkey.connect()
-        nonlocal connect_waiting_timer
         connect_waiting_timer = threading.Timer(0.5,connect_waiting)
         connect_waiting_timer.start()
         #send('connect',0,0,0,0,0,0,0,0)
     connect_waiting_timer = threading.Timer(0.5,connect_waiting)
-    def pause():
+    def pause(self):
         #print_text.insert('end',"TestMethod: pause")
         #send('pause',0,0,0,0,0,0,0,0)
         Monkey.pause()
 
-    def resume():
+    def resume(self):
         #print_text.insert('end',"TestMethod: resume")
         #send('resume',0,0,0,0,0,0,0,0)
         Monkey.resume()
 
-    def stop():
+    def stop(self):
         #print_text.insert('end',"TestMethod: stop")
         #send('stop',0,0,0,0,0,0,0,0)
         Monkey.stop()
 
-    def start():
-        log_timer = threading.Timer(1,log_monitor)
-        log_timer.start()
+    def start(self):
+        #log_timer = threading.Timer(1,log_monitor)
+        #log_timer.start()
         #print_text.insert('end',"TestMethod: start")
         #send('start',0,0,0,0,0,0,0,0)
         Monkey.start()
--- GUI/pyqt/test_pyqt.py
+++ GUI/pyqt/test_pyqt.py
@@ -6,17 +6,18 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import QFileDialog, QMessageBox, QDockWidget, QListWidget
 from PyQt5.QtGui import *
 #from Test_Ui_Functions import *
-import Test_Ui_Functions
-from Ui_test_ui import Ui_TestWindow #import刚刚创建的GUI类
+import Test_Ui_Functions as funcs
+from Ui_test_ui_1 import Ui_TestWindow #import刚刚创建的GUI类
 class myTestWindow(QtWidgets.QMainWindow,Ui_TestWindow):
     def __init__(self):#因为是继承自Qtwidgets.QMainWindow和Ui_MainWindow的，重载一下父类构造函数
         super(myTestWindow,self).__init__()
         self.setupUi(self) #初始化UI界面
-        #self.pushButton.clicked.connect(self.hello_message)
         self.queueList.setDragDropMode(self.queueList.InternalMove)
-        '''for i in range(0,5):
-            self.queueList.addItem("hello")
-        self.queueList.addItem("world")'''
+        self.queueList.setAcceptDrops(True)
+        for i in range(0,5):
+            self.queueList.addItem(str((i * 753 + 33) % 14))
+        self.queueList.addItem("world")
+
 
 if  __name__ == '__main__': #python的main函数
     app = QtWidgets.QApplication(sys.argv)
@@ -24,6 +25,8 @@ if  __name__ == '__main__': #python的main函数
     # 对于GUI程序必须至少有一个这样的实例来让程序运行。
     test_window = myTestWindow()
     test_window.show()
+    fuctions_class = funcs.TestUiFunctionsClass()
+    fuctions_class.add_text("12345",test_window.queueList)
     #close_monkeyrunner()
     #close_model()
     sys.exit(app.exec_()) #正常退出程序
