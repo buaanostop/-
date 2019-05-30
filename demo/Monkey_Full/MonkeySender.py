a0f0473583770d7bf8def66f9cd900460e70cda3
bhlt1998 <34224727+bhlt1998@users.noreply.github.com>
2019-05-30 14:41:13 +0800

    添加返回正在执行的测试 Monkey.now_running()
    ### 11.返回正在进行的测试
    now_running()
        返回值
        int : 测试的序号 0开始，在没有开始或已经结束的情况下返回-1
    -------------------------------------

--- demo/Monkey_Full/MonkeySender.py
+++ demo/Monkey_Full/MonkeySender.py
@@ -24,6 +24,8 @@ resolution_ratio = (540,960)
 root_path = os.getcwd()
 shot_path = root_path + '\screenshot'
 log_path = 'log.txt'
+
+now_index = -1
 class do_run_monkey(threading.Thread):
     
     def __init__(self, monkeypath):
@@ -67,7 +69,11 @@ def do_close():
     sendsocket.sendto(data, (host, port))
     recsocket.close()
     print("close")
-    
+
+def do_now_running():
+    global now_index
+    return now_index
+
 class DoTest(threading.Thread):
     def __init__(self, oplist):
         threading.Thread.__init__(self)
@@ -358,10 +364,13 @@ class DoTest(threading.Thread):
         self.__send('clearshot', 0, 0, 0, 0, 0, 0)
         
     def run(self):
+        global now_index
         self.flag.set()
         self.stopflag.clear()
         self.__clearshot()
         for op in self.oplist:
+            now_index += 1
             if self.stopflag.isSet():
                 break
             self.__actOp(op)
+        now_index = -1
